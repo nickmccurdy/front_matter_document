8e83c35cbd69b983299ef0cf03a69967375297e5
Nicolas McCurdy <thenickperson@gmail.com>
2015-05-24 16:55:27 -0400

    Use RSpec subjects

--- spec/front_matter_ninja_spec.rb
+++ spec/front_matter_ninja_spec.rb
@@ -9,9 +9,11 @@ describe FrontMatterNinja do
   end
 
   describe '.parse' do
+    subject { described_class.parse doc }
+
     context 'given some front matter' do
-      it 'returns a Hash of the front matter' do
-        doc = %(
+      let(:doc) do
+        %(
           ---
           one: 1
           two: 2
@@ -19,27 +21,33 @@ describe FrontMatterNinja do
 
           Hello, world!
         ).strip_heredoc.strip
+      end
 
-        expect(described_class.parse(doc)).to eq('one' => 1, 'two' => 2)
+      it 'returns a Hash of the front matter' do
+        is_expected.to eq('one' => 1, 'two' => 2)
       end
     end
 
     context 'given some empty front matter' do
-      it 'returns an empty Hash' do
-        doc = %(
+      let(:doc) do
+        %(
           ---
           ---
 
           Hello, world!
         ).strip_heredoc.strip
+      end
 
-        expect(described_class.parse(doc)).to eq({})
+      it 'returns an empty Hash' do
+        is_expected.to eq({})
       end
     end
 
     context 'given no front matter' do
+      let(:doc) { 'Hello, world!' }
+
       it 'returns nil' do
-        expect(described_class.parse('Hello, world!')).to be_nil
+        is_expected.to be_nil
       end
     end
   end
